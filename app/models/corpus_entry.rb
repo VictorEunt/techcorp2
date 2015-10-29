e3c410a364ae1d99016fc4504a60feaf27e86c2a
VictorEunt <VictorEstrada@my.unt.edu>
2015-10-29 01:39:28 -0500

    updated view to link to uploaded documents

--- app/models/corpus_entry.rb
+++ app/models/corpus_entry.rb
@@ -2,5 +2,6 @@ class CorpusEntry < ActiveRecord::Base
   belongs_to :member
   
   has_attached_file :document
-  validates_attachment_content_type :document, :content_type => ["application/pdf"]
+  validates_attachment :document, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
+
 end
